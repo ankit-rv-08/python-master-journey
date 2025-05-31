87910596fc2569cc5f15be5063e7f4c28a6e8b35
ankit-rv-08 <ankith8804@gmail.com>
2025-05-31 22:41:46 +0530

    Update hour1_python_basics.py

--- hour1_python_basics.py
+++ hour1_python_basics.py
@@ -1,3 +1,8 @@
+"""
+Hour 1: Python Basics
+Author: Ankith
+Description: Script to practice variables, input/output, and type conversion.
+"""
 name = input("Enter your name :  ")
 print(f"Welcome {name}!")
 
